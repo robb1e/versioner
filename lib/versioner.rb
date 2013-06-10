36406f8ebdea8a110e5a1595d821ea716c159e77
Robbie Clutton <robert.clutton@gmail.com>
2013-06-09 22:05:19 -0400

    refactor

--- lib/versioner.rb
+++ lib/versioner.rb
@@ -3,16 +3,6 @@ require "versioner/version"
 module Versioner
   def self.find_version_for version, list
     return list.last if version.nil?
-
-    selected_version = nil
-    list.each_cons(2) do |previous_version, next_version|
-      selected_version = next_version
-      if next_version > version
-        selected_version = previous_version
-        break
-      end
-    end
-
-    selected_version
+    list.select { |i| i <= version }.last || list.first
   end
 end
